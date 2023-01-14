7e3f7da92424d60ef5d33683fd7e61060d436ea3
lifavo <ea.zchaos@gmail.com>
2023-01-14 11:57:53 -0500

    post request to access spotify apis

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import requests
 
-url = 'https://www.w3schools.com/python/demopage.php'
-myobj = {'somekey': 'somevalue'}
+CLIENT_ID = '.'
+CLIENT_SECRET = '.'
 
-x = requests.post(url, json=myobj)
+AUTH_URL = 'https://accounts.spotify.com/api/token'
 
-print(x.text)
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+# base URL of all Spotify API endpoints
+BASE_URL = 'https://api.spotify.com/v1/'
+
+# Track ID from the URI
+# track_id = '6y0igZArWVi6Iz0rj35c1Y'
+
+# actual GET request with proper header
+r = requests.get(BASE_URL + 'me/player/recently-played', headers=headers)
+
+r = r.json()
+print(r)
